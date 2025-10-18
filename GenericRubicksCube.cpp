35214aa968641c714cd66ca78b06008efc41bb8b
Nishkarsh Sharma <nishkarshsharma2004@gmail.com>
2025-10-18 11:39:07 +0530

    Added GenericRubicksCube Model

---
+++ GenericRubicksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nishkarsh Sharma on 18-10-2025.
+//
+
+#include "GenericRubicksCube.h"
---
+++ GenericRubicksCube.h
@@ -0,0 +1,13 @@
+//
+// Created by Nishkarsh Sharma on 18-10-2025.
+//
+
+#ifndef RUBIK_S_CUBE_GENERICRUBICKSCUBE_H
+#define RUBIK_S_CUBE_GENERICRUBICKSCUBE_H
+
+
+class GenericRubicksCube {
+};
+
+
+#endif //RUBIK_S_CUBE_GENERICRUBICKSCUBE_H
